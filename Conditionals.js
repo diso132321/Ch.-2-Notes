3f6f96d576a6283a89fa9cba99eb6cb750842ea3
u4h8 <u4h8@fresnoU.org>
2023-02-08 09:41:21 -0800

    Ch. 2 Notes

---
+++ Conditionals.js
@@ -0,0 +1,25 @@
+// Conditionals are if-statements
+
+let x = 5;
+if (x == 5){
+    console.log("it's 5!");
+}
+console.log("end of program");
+let y = 10;
+if (y == 5){
+    console.log("it's 5!");
+}
+console.log("end of program");
+
+if (y == 5){
+    console.log("it's 5!");
+}
+console.log("end of program");
+
+let z = 20;
+if (z == 5){
+    console.log("option1");
+} else {
+    console.log("option2");
+}
+console.log("end of program");
