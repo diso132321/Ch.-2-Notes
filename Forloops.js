0e0145586ae5a26f0c0ca71ce8fc5c53b1264029
u4h8 <u4h8@fresnoU.org>
2023-02-24 10:27:57 -0800

    Ch. 2 Notes Updated Input and Jokes Forloops and WhileLoops

---
+++ Forloops.js
@@ -0,0 +1,14 @@
+// For Loops
+// for(let i = 0; i<=10; i++){
+// console.log(i);
+// }
+
+// for(let i = 0; i<=1000; i++){
+// console.log(i);
+// }
+
+let sum = 0;
+for (let i = 0; i <= 100; i++) {
+    if(1 % 2 == 0) {
+        console.log('sum: ${sum}');
+    }
